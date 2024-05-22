c1296dd32db7ba51feaaffbc38e712fc0542419e
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2024-05-22 18:49:01 +0800

    [backend/handler] Set request handler to correctly process `www-form-urlencoded` content-type

--- backend/handler/request.php
+++ backend/handler/request.php
@@ -4,13 +4,14 @@ class RequestHandler {
     public $request;
     public $connection;
     public string $body = "";
+    public string $content_type;
 
     function __construct($request, mysqli | PDO | null $connection = null){
-        # ref: https://stackoverflow.com/questions/8945879/how-to-get-body-of-a-post-in-php
-        $this->body = file_get_contents("php://input");
-        
+        # ref: https://stackoverflow.com/questions/8945879/how-to-get-body-of-a-post-in-php        
         $this->request = $request;
         $this->connection = $connection;
+        $this->content_type = $this->request['CONTENT_TYPE'];
+        $this->body = file_get_contents("php://input");
     }
 
     public function GET(){
@@ -35,9 +36,33 @@ class RequestHandler {
 
 }
 
+function convert_form_data_to_array(string $formData){
+    $ampersand_split = explode("&", $formData);
+    $result_array = array();
+    foreach ($ampersand_split as $first_split){
+        $second_split = explode("=", $first_split);
+        $key = $second_split[0];
+        $value = $second_split[1];
+
+        array_push($result_array, array(
+            $key => $value
+        ));
+    };
+    $result_array_2 = array_merge(...$result_array);
+
+    return $result_array_2;
+}
+
 function handle_request(RequestHandler &$handler){
     $http_method = $handler->request['REQUEST_METHOD'];
-
+    
+    // Convert the form data into json if content-type is application/x-www-form-urlencoded
+    if (preg_match('/application\/x-www-form-urlencoded/', $handler->content_type)){
+        $handler->body = json_encode(
+            convert_form_data_to_array(
+                urldecode($handler->body))
+        );
+    }
     $response = null;
     match ($http_method) {
         "GET" => ($response = $handler->GET()),
