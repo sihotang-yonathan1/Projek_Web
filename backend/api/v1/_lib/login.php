dabd0ecf98e16f296c6df94865df5c404093b275
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2024-05-23 14:45:05 +0800

    [api/_lib] Move login handler to its own file

---
+++ backend/api/v1/_lib/login.php
@@ -0,0 +1,28 @@
+<?php
+
+class LoginHandler extends RequestHandler {
+    function POST(){
+        // get the request body
+        $req = json_decode($this->body, true);
+        
+        # ref: https://www.w3schools.com/php/php_mysql_prepared_statements.asp
+        $prepared_statement = $this->connection->prepare("
+            SELECT id, nama, user_type FROM user_form WHERE email=? AND password=?
+        ");
+        
+        $prepared_statement->bind_param('ss', ...[
+            $req['email'], $req['password']
+        ]);
+        
+        $prepared_statement->execute();
+
+        $result = $prepared_statement->get_result();
+
+        if ($result->num_rows == 1){
+            $temp_result = mysqli_fetch_assoc($result);
+            mysqli_free_result($result);
+            return $temp_result;
+        }
+        return $req;
+    }
+}
