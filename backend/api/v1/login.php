8f8f6f3cf96ef248da2aa4f297f3022a8b32c00c
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2024-05-23 14:45:49 +0800

    [api] fix duplicated handler

--- backend/api/v1/login.php
+++ backend/api/v1/login.php
@@ -4,35 +4,9 @@ session_start();
 $PROJECT_DIRECTORY = dirname(__FILE__, 4);
 
 require_once($PROJECT_DIRECTORY . "/backend/connect.php");
-require_once( $PROJECT_DIRECTORY . "/backend/handler/request.php");
-require_once( $PROJECT_DIRECTORY . "/backend/handler/response.php");
-
-class LoginHandler extends RequestHandler {
-    function POST(){
-        // get the request body
-        $req = json_decode($this->body, true);
-        
-        # ref: https://www.w3schools.com/php/php_mysql_prepared_statements.asp
-        $prepared_statement = $this->connection->prepare("
-            SELECT id, nama, user_type FROM user_form WHERE email=? AND password=?
-        ");
-        
-        $prepared_statement->bind_param('ss', ...[
-            $req['email'], $req['password']
-        ]);
-        
-        $prepared_statement->execute();
-
-        $result = $prepared_statement->get_result();
-
-        if ($result->num_rows == 1){
-            $temp_result = mysqli_fetch_assoc($result);
-            mysqli_free_result($result);
-            return $temp_result;
-        }
-        return $req;
-    }
-}
+require_once($PROJECT_DIRECTORY . "/backend/handler/request.php");
+require_once($PROJECT_DIRECTORY . "/backend/handler/response.php");
+require_once($PROJECT_DIRECTORY . "/backend/api/v1/_lib/login.php");
 
 
 $handler = new LoginHandler($_SERVER, $connection);
