ea93924d05b8830133e8f191a8a86979b4f449fc
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2024-05-23 14:46:00 +0800

    [api] fix dupliacted handler

--- backend/api/v1/user.php
+++ backend/api/v1/user.php
@@ -6,26 +6,7 @@ require_once($PROJECT_DIRECTORY . "/backend/connect.php");
 require_once( $PROJECT_DIRECTORY . "/backend/handler/request.php");
 require_once( $PROJECT_DIRECTORY . "/backend/handler/response.php");
 
-
-class UserHandler extends RequestHandler {
-    function POST(){
-        // get the request body
-        $req = json_decode($this->body, true);
-        
-        # ref: https://www.w3schools.com/php/php_mysql_prepared_statements.asp
-        $prepared_statement = $this->connection->prepare("
-            INSERT INTO user_form (nama, email, password, user_type)
-            VALUE (?, ?, ?, ?)
-        ");
-        
-        $prepared_statement->bind_param('ssss', ...[
-            $req['nama'], $req['email'], $req['password'], $req['user_type']
-        ]);
-        
-        $prepared_statement->execute();
-        return $req;
-    }
-}
+require_once($PROJECT_DIRECTORY . "/backend/api/v1/_lib/user.php");
 
 
 $handler = new UserHandler($_SERVER, $connection);
