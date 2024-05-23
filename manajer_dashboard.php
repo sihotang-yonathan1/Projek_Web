af46c793f814c1450186b85b50bb1bad589bfc66
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2024-05-23 20:27:39 +0800

    [page:manajer_dashboard] Refactor and use api instead defining manually

--- manajer_dashboard.php
+++ manajer_dashboard.php
@@ -1,36 +1,17 @@
 <?php
 session_start();
 
+require_once('./config.php');
+require_once('./utils/network/http_client.php');
+
 if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
     // Pengguna belum login, arahkan ke halaman login
     header("Location: index.php");
     exit();
 }
-
-// TODO: encapsulate the DB connection process in one separate file
-// Koneksi ke database (ganti dengan detail koneksi Anda)
-$host = "localhost";
-$username = "root";
-$password = "";
-$db = "user_db";
-
-$conn = new mysqli($host, $username, $password, $db);
-
-// Periksa koneksi
-if ($conn->connect_error) {
-    die("Koneksi Gagal: " . $conn->connect_error);
-}
-
-// Query untuk mendapatkan data reservasi
-$query = "SELECT * FROM reservasi_form";
-$result = $conn->query($query);
-// Ambil data reservasi ke dalam array
-$reservasiData = array();
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $reservasiData[] = $row;
-    }
-}
+$max_items_number = 3;
+$_result = HttpClient::get("$PROJECT_URL/backend/api/v1/reservasi.php?max_items_number=$max_items_number");
+$result = json_decode($_result, true);
 
 ?>
 
@@ -93,22 +74,15 @@ header("Content-Type: text/html; charset=UTF-8");
                             <th>Jenis Meja</th>
                             <th>Status</th>
                         </tr>
-                        <?php
-                        $counter = 0;
-                        foreach ($reservasiData as $row) {
-                            echo "<tr>
-                                <td>" . $row['tanggal'] . "</td>
-                                <td>" . $row['waktu'] . "</td>
-                                <td>" . $row['jumlah_orang'] . "</td>
-                                <td>" . $row['jenis_meja'] . "</t>
-                                <td>" . ($row['status'] ?? 'Belum Konfirmasi') . "</td>
-                                </tr>";
-                            $counter++;
-                            if ($counter >= 3) {
-                                break; // Hentikan iterasi setelah tiga baris
-                            }
-                        }
-                        ?>
+                        <?php foreach ($result as $data): ?>
+                            <tr>
+                                <td><?= $data['tanggal'] ?></td>
+                                <td><?= $data['waktu'] ?></td>
+                                <td><?= $data['jumlah_orang'] ?></td>
+                                <td><?= $data['jenis_meja'] ?></td>
+                                <td><?= $data['status'] ?? 'Belum konfigurasi' ?></td>
+                            </tr>
+                        <?php endforeach ?>
                     </table>
                 </div>
 
@@ -121,22 +95,12 @@ header("Content-Type: text/html; charset=UTF-8");
                             <th>Nama Pelanggan</th>
                             <th>Status</th>
                         </tr>
-                        <?php
-                        $counter = 0;
-                        foreach ($reservasiData as $row) {
-                            echo "<tr>
-                                <td>" . $row['nama'] . "</td>
-                                <td>" . ($row['status'] ?? 'Belum Konfirmasi') . "</td>
-                                </tr>";
-                            $counter++;
-                            if ($counter >= 3) {
-                                break; // Hentikan iterasi setelah tiga baris
-                            }
-                        }
-
-                        //close koneksi
-                        $conn->close();
-                        ?>
+                        <?php foreach ($result as $data): ?>
+                            <tr>
+                                <td><?= $data['nama']?></td>
+                                <td><?= $data['status'] ?? 'Belum Konfirmasi' ?></td>
+                            </tr>
+                        <?php endforeach ?>
                     </table>
                 </div>
             </div>
