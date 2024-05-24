35b1f1303421d886c420989b6fab6fd8240c6734
keqi000 <144864650+keqi000@users.noreply.github.com>
2024-05-24 20:19:57 +0800

    refactor script.js 1

--- users/beranda/script.js
+++ users/beranda/script.js
@@ -1,61 +1,10 @@
 /* ----- BASE ------ */
 "use strict";
 
-// get required selectors to maniplute menu toggle
-const navbar = document.querySelector (".navbar");
-const menuTogglersContainer = document.querySelector(".menu-togglers");
-const bxMenu = document.querySelector(".bx-menu");
-
-/* -- show/hide menu -- */
-menuTogglersContainer.addEventListener("click", () => {
-  // if navbar tag have show-nav in as class
-  navbar.classList.toggle("show-nav");
-});
-
 // tombol buttom
 function logout() {
-  window.location.href = "/rpl-project/index.php";
+  window.location.href = "index.php";
 }
-
-
-// disable dark mode function
-const disableLightMode = () => {
-  // remove class dark mode from the body
-  document.body.classList.remove("lightmode");
-  localStorage.setItem("lightmode", null);
-  // change theme toggle styles
-  lightIcon.style.display = "block";
-  darkIcon.style.display = "none";
-};
-
-// active/deactive dark mode
-themeTogglers.addEventListener("click", () => {
-  lightmode = localStorage.getItem("lightmode");
-  if (!lightmode || lightmode !== "enabled") {
-    enableLightMode();
-  } else {
-    disableLightMode();
-  }
-});
-
-
-/* -- hide show hero buttons -- */
-// delay before showing them
-const heroButtonsContainer = document.querySelector(".hero-btns-container");
-
-var delayTime = 1000;
-
-heroButtonsContainer.style.transition = "opacity 1000ms";
-
-setTimeout(() => {
-  heroButtonsContainer.style.opacity = 1;
-}, delayTime);
-
-// --- prevent form submission on contact section ---
-const sendMsgButton = document.querySelector(".send-msg-btn");
-sendMsgButton.addEventListener("click", (e) => {
-  e.preventDefault();
-});
 
 function logout() {
   // Lakukan proses logout di sini
