4d5aea74838749e5e9b4ed4a7599c6a9667ec6c5
keqi000 <144864650+keqi000@users.noreply.github.com>
2024-05-25 14:21:51 +0800

    Refactor manajer_dashboard.php 1

--- manajer_dashboard.php
+++ manajer_dashboard.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
 $host = "localhost";
 $username = "root";
 $password = "";
-$db = "user_db";
+$db = "reservasi_form";
 
 $conn = new mysqli($host, $username, $password, $db);
 
@@ -42,9 +42,8 @@ header("Content-Type: text/html; charset=UTF-8");
 <head>
     <meta charset="UTF-8" />
     <title>Dashboard Manajer</title>
-    <link rel="stylesheet" href="manajer/style.css" />
-    <link rel="icon" href="/rpl-project/image/img.jpg">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+    <link rel="stylesheet" href="manager/style.css" />
+    <link rel="icon" href="gambar/img.jpg">
 </head>
 
 <body>
@@ -53,7 +52,6 @@ header("Content-Type: text/html; charset=UTF-8");
             <a href="#">WarongWarem</a>
             <div class="search_box">
                 <input type="text" placeholder="Search ">
-                <i class="fas fa-search"></i>
             </div>
         </div>
 
@@ -142,7 +140,7 @@ header("Content-Type: text/html; charset=UTF-8");
         </div>
     </div>
 
-    <script src="manajer/script.js"></script>
+    <script src="manager/script.js"></script>
 </body>
 
 </html>
