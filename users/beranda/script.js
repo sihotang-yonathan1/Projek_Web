f7efe4eaa6843cb7b5a0f67ed7bc0eb5ddd473e4
keqi000 <144864650+keqi000@users.noreply.github.com>
2024-05-26 17:49:05 +0800

    Refactor script.js 3

--- users/beranda/script.js
+++ users/beranda/script.js
@@ -11,3 +11,24 @@ function logout() {
   // Hapus sesi dan arahkan ke halaman login
   window.location.href = "logout.php";
 }
+
+document.querySelectorAll(".nav-link").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    // Periksa apakah tautan memiliki hash dalam href
+    if (item.getAttribute("href").charAt(0) === "#") {
+      event.preventDefault(); // Mencegah perilaku default dari tautan
+
+      const targetId = item.getAttribute("href"); // Ambil id target dari href
+      const target = document.querySelector(targetId);
+      const headerHeight = document.querySelector("header").offsetHeight; // Tinggi header
+
+      if (target) {
+        // Gulir halaman ke bagian awal elemen target dengan memperhitungkan tinggi header
+        window.scrollTo({
+          top: target.offsetTop - headerHeight,
+          behavior: "smooth", // Efek gulir halus
+        });
+      }
+    }
+  });
+});
